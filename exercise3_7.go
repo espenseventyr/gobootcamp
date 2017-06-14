18cbc31356284398c29636bbdcab57e295594bed
espenseventyr <espen.trydal@gmail.com>
2017-06-14 08:38:02 +0000

    first commit

---
+++ exercise3_7.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Id             int
+	Name, Location string
+}
+
+func (u *User) Greetings() string {
+	return fmt.Sprintf("Hi %s from %s",
+		u.Name, u.Location)
+}
+
+type Player struct {
+	*User
+	GameId int
+}
+
+func main() {
+	p := &Player{}
+	fmt.Println(*p.Greetings{})
+}
